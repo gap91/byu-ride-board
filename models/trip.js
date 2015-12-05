5f18ebda4b3ff48a11b405980dfef6bb2f229fdc
mbenne49 <skibum909@gmail.com>
2015-12-05 14:30:06 -0700

    adding the trip schema for the mongo db.

---
+++ models/trip.js
@@ -0,0 +1,31 @@
+// setup Mongoose
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var ObjectId = Schema.ObjectId;
+var findOrCreate = require('mongoose-findorcreate')
+
+    var User = require('./user.js');
+
+// Trip schema
+var tripSchema = new Schema({
+    users: [{type: ObjectId, ref: 'users'}], // need to make this a list
+    destination: String,
+    leaving: Date,
+    returning: Date,
+    contact: String,
+    Discription: String,
+    seats: int,
+});
+
+// ensure shemas use virtual IDs
+tripSchema.set('toJSON', {
+    virtuals: true
+});
+
+// add findorCreate
+tripSchema.plugin(findOrCreate);
+
+// create trip
+var Trip = mongoose.model('trips', tripSchema);
+
+module.exports = Trip;
